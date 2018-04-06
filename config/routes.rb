47df1a9ca8a14c567a792dbf114d7b0607d1ee0b
Unknown <jgordola@academic.rrc.ca>
2018-04-06 11:26:50 -0500

    added path for cart orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  root to: 'products#index'
   resources :abouts, only: [:show]
   resources :products, only: [:index, :show]
 
@@ -7,7 +8,9 @@ Rails.application.routes.draw do
   get 'search', to: 'search#index', as: 'search'
   get 'category/:id', to: 'categories#show', as: 'category'
 
-  root to: 'products#index'
+
+  get '/cart', to: 'order_items#index'
+  resources :order_items, path: '/cart/items'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
