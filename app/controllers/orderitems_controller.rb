e1479090539f75dc327a6da0ebaeac4377f70596
Unknown <jgordola@academic.rrc.ca>
2018-04-06 13:12:13 -0500

    Deleted a def at OrderItems controller.

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -2,7 +2,6 @@ class OrderitemsController < ApplicationController
   def create
     current_cart.add_item(product_id: params[:product_id],
                           quantity: params[:quantity])
-
     redirect_to cart_path
   end
 end
