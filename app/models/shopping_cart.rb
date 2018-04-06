4c12801fd6c2876d3ba0a23ba783fc6e052b7b32
Unknown <jgordola@academic.rrc.ca>
2018-04-06 11:52:22 -0500

    Fixed shopping cart model.

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -2,10 +2,5 @@ class ShoppingCart < ApplicationRecord
   def initialize(token:)
     @token = token
   end
-
-  def order
-    @order ||= Order.find_or_create_by(:token @token) do |order|
-    order.sub_total = 0
-    end
-  end
+  
 end
