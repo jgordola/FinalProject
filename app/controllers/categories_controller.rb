e7b984c82ed5ee8151cde636dacf66a47c9f0bf0
Unknown <jgordola@academic.rrc.ca>
2018-04-12 09:12:17 -0500

    Created flash message for categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,8 @@
 class CategoriesController < ApplicationController
   def show
     @category_terms = params[:id]
+    cat = Category.find(params[:id])
+    flash[:message] = "You've selected category:  #{cat.name}"
     @products = Product.where(category_id: @category_terms).order(id: :desc)
   end
 end
